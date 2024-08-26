06be6fb95290f964c3858aae1062ef513fa96a57
brgomesz <bruno.rodrigues.gomes@hotmail.com>
2024-08-26 20:46:31 -0300

    feature: adicionei o servidor local

---
+++ src/app/local-storage.service.ts
@@ -0,0 +1,49 @@
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class LocalStorageService {
+
+//   constructor() { }
+// }
+
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+  private isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  setItem(key: string, value: any): void {
+    if (this.isBrowser) {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  }
+
+  getItem(key: string): any {
+    if (this.isBrowser) {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : null;
+    }
+    return null;
+  }
+
+  removeItem(key: string): void {
+    if (this.isBrowser) {
+      localStorage.removeItem(key);
+    }
+  }
+
+  clear(): void {
+    if (this.isBrowser) {
+      localStorage.clear();
+    }
+  }
+}
